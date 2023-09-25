6b136593d687cae345759825bc62ca142da3c03a
eclonghurst <emily.longhurst3@sky.uk>
2023-09-25 16:28:34 +0100

    white footer text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,13 +8,13 @@ function Footer(){
             <div className="container">
   <footer className="py-3 my-4">
     <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary white-footer-text">Home</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary white-footer-text">About</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary white-footer-text">Privacy Policy</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary white-footer-text">Terms of service</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary white-footer-text">Contact us</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">Home</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">About</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">Privacy Policy</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">Terms of service</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">Contact us</a></li>
     </ul>
-    <p className="text-center text-body-secondary white-footer-text"> &copy; GetYourWay.com, Inc</p>
+    <p className="text-center white-footer-text"> &copy; GetYourWay.com, Inc</p>
   </footer>
 </div>
       
