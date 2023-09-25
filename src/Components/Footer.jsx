ad2470270759bbc2f4908c047830c18ce79019f7
Fayequinn <fayequinn88@hotmail.com>
2023-09-25 21:09:45 +0100

    made the footer stick to the bottom but there could be a better way? I have made notes in css file for footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(){
     return(
       
             <div className="container">
-  <footer className="py-3 my-4">
+  <footer className="fixed-bottom py-3 my-4">
     <ul className="nav justify-content-center border-bottom pb-3 mb-3">
       <li className="nav-item"><a href="home" className="nav-link px-2 white-footer-text">Home</a></li>
       <li className="nav-item"><a href="#" className="nav-link px-2 white-footer-text">About</a></li>
