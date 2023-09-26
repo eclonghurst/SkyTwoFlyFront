63dd4e1a27fe6561a2e75ac0105fc1695292df97
Salma394 <salmaibrahim813@gmail.com>
2023-09-26 19:21:32 +0100

    added form to registration

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Form from '../Components/RegistrationForm'
+import RegistrationForm from "../Components/RegistrationForm";
 function SignUp(){
-    return<h1>Sign Up!</h1>
+    return<>
+    <h1>Register</h1>
+    <RegistrationForm/>
+</>
+
+
+
+
 }
 
 export default SignUp;
