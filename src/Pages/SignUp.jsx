db1169863adb9210a9ea40a50d80b2123c9f6917
eclonghurst <emily.longhurst3@sky.uk>
2023-09-27 16:09:39 +0100

    centered sign up

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Form from '../Components/RegistrationForm'
 import RegistrationForm from "../Components/RegistrationForm";
+import '../CssFiles/RegistrationFormCSS.css'
+
 function SignUp(){
-    return<>
-    <h1>Register</h1>
-    <RegistrationForm/>
-</>
-
-
-
-
+    
+    return(
+    <>
+        <h1 className="registerTitle">Register</h1>
+        <RegistrationForm/>
+    </>
+    )
 }
 
 export default SignUp;
