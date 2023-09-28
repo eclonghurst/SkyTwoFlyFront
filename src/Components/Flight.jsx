edf9d102971cd6696a113cbf3d9a5fdb7e015d24
eclonghurst <emily.longhurst3@sky.uk>
2023-09-28 16:29:11 +0100

    flights css

--- src/Components/Flight.jsx
+++ src/Components/Flight.jsx
@@ -7,17 +7,16 @@ function Flight(){
 
     return(
         <>
-        <div className='container-flight'>
+        <div className='flight-panel'>
             <div className='container-airline-details'>
                 <img src={airlineLogo} alt="Airline logo"/> 
-
             </div>
             <div className='container-flight-details'>
-<p>Travel details</p>
+            <p>Travel details</p>
             </div>
             <div className='container-flight-details-price'>
 <p>££</p>
-<button className='book-button'>Book</button>
+                    <button className='book-button'>Book</button>
             </div>
 
 
--- src/Components/FlightsPage.jsx
+++ src/Components/FlightsPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Flight from "./Flight";
 import Weather from "./Weather";
+import "../CssFiles/FlightPageCSS.css"
 
 function FlightsPage(){
 
     return(
         <>
-<div style="display: flex">
-    <Flight/>
+<div className="flight-page-container">
+    <div className="flight-container">
+        <Flight/>
+        <Flight/>
+    </div>
     <Weather/>
 
 </div>
