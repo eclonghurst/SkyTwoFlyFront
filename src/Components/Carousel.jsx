d105f1624a321a30e06aedc0442f19104a8096bf
eclonghurst <emily.longhurst3@sky.uk>
2023-09-28 17:24:35 +0100

    carousel etc

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,33 +1,40 @@
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import '../CssFiles/CarouselCSS.css'
 
 const images = [
   {
     original: "/kashmir.jpg",
     description: "Pakistan",
+    originalHeight: "600px",
+    originalWidth: "1000px",
   },
   {
     original: "/japan.jpg",
     description: "Japan",
+    originalHeight: "600px",
   },
   {
     original: "/italy.jpg",
     description: "Italy",
+    originalHeight: "600px",
   },
   {
     original: "/china.jpg",
     description: "China",
+    originalHeight: "600px",
   },
   {
     original: "/austria.jpg",
     description: "Austria",
+    originalHeight: "600px",
   },
 ];
 
 const Carousel = () => {
   return (
-    <div>
-      <ImageGallery items={images} />
+    <div className="carousel-container">
+      <ImageGallery items={images} showBullets={true}/>
     </div>
   );
 };
