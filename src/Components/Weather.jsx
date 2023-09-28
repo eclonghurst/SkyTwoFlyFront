12e406d517160311a72a056d43362e0a4bf964c7
Fayequinn <fayequinn88@hotmail.com>
2023-09-28 22:34:31 +0100

    called a weather api and displayed response in weather section

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 import "../CssFiles/Weather.css"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function Weather(){
+
+const [weather, setWeather] = useState('');
+
+const [locationDetails, setLocationDetails] = useState('');
+
+const getWeather = function(){
+    axios.get("http://api.weatherapi.com/v1/current.json?key=38387323c061422fae1200751232809&q=Rhodes")
+    .then(function(response){
+        console.log("RESPONSE RECEIVED");
+        setWeather({condition: response.data.current.condition.text,
+        tempC: response.data.current.temp_c,
+    tempF: response.data.current.temp_f});
+setLocationDetails({city: response.data.location.name, country:response.data.location.country})})
+    .catch(err => console.error(err));   
+}
+
+useEffect(function(){
+    getWeather();
+},[]);
+
+
     return(
         <>
         <div className="weather">
-        <p>Weather</p>
+        <h2>Weather at your chosen destination...</h2>
+        <h3>{locationDetails.city}, {locationDetails.country}</h3>
+        <table>
+            <tr>
+                <th>Condition:  </th>
+                <td> {weather.condition} </td>
+            </tr>
+            <tr>
+                <th>Temperature C:     </th>
+                <td> {weather.tempC} </td>
+            </tr>
+            <tr>
+            <th>Temperature F:     </th>
+                <td> {weather.tempF} </td>
+            </tr>
+        </table>
         </div>
         </>
     )
