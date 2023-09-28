982b840e9416adceeadd13a8c6184afb2fc152fd
Salma394 <salmaibrahim813@gmail.com>
2023-09-28 19:26:35 +0100

    trying to make the Sign up a pop up

--- src/Components/RegistrationForm.jsx
+++ src/Components/RegistrationForm.jsx
@@ -2,52 +2,73 @@ import React, { useState } from 'react';
 import '../CssFiles/RegistrationFormCSS.css';
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-  };
-
-  return (
-    <div className="form-container" style={{ marginTop: '0px' }}>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="firstName" className="form-label">First Name</label>
-          <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName" className="form-label">Last Name</label>
-          <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} className="form-control" />
-        </div>
-        <button type="submit" className="btn btn-primary">Register</button>
-      </form>
-    </div>
-  );
-}
-
-
+  console.log('Registerastion is rendered')
   
+  const [firstName,setFirstName]= useState("")
+  const [lastName,setLastName]= useState("")
+  const [email,setEmail]= useState("")
+  const [password,setPassword]= useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+  };
+
+
+return(
+<div className='backdrop'>
+  <div className='modal'>
+
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input 
+        type = "text"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}/>
+
+      </label>
+      <br/>
+      <label>
+        Last Name:
+        <input 
+        type = "text"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}/>
+
+      </label>
+      <br/>
+      <label>
+        Email:
+        <input 
+        type = "text"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}/>
+      </label>
+
+      <br/>
+      <label>
+        Password:
+        <input 
+        type = "text"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}/>
+
+      </label>
+      <br/>
+      <button type = "submit">Submit</button>
+    </form>
+  </div>
+
+
+
+</div>
+
+)
+
+};
 
 export default RegistrationForm;
