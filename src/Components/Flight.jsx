3f4e6d30a691688ba8942a09611fedea20c7330e
Fayequinn <fayequinn88@hotmail.com>
2023-09-29 15:26:20 +0100

    a bit of tidying to the flight panel

--- src/Components/Flight.jsx
+++ src/Components/Flight.jsx
@@ -1,36 +1,30 @@
 import React from "react";
-import '../CssFiles/FlightCSS.css';
-import airlineLogo from '../Images/airlineLogo.png'
+import "../CssFiles/FlightCSS.css";
+import airlineLogo from "../Images/airlineLogo.png";
+import aircraft from "../Images/aircraft.png";
 
-
-function Flight(){
-
-    return(
-        <>
-        <div className='flight-panel'>
-            <div className='container-airline-details'>
-                <img src={airlineLogo} alt="Airline logo" className="airline-logo"/> 
-            </div>
-            <div className='container-flight-details'>
-            <div className='departure'>
-MAN
-            </div>
-            <div className='flight-pointer'>
-
-            </div>
-            <div className='destination'>
-DUS
-            </div>
-            </div>
-            <div className='container-flight-details-price'>
-<p>££</p>
-                    <button className='book-button'>Book</button>
-            </div>
-
-
+function Flight() {
+  return (
+    <>
+      <div className="flight-panel">
+        <div className="container-airline-details">
+          <img src={airlineLogo} alt="Airline logo" className="airline-logo" />
         </div>
-        </>
-    )
+        <div className="container-flight-details">
+          <div className="departure">MAN</div>
+
+          <div className="'flight-route">
+            <img src={aircraft} alt="aircraft" className="aircraft-icon"/>
+          </div>
+          <div className="destination">DUS</div>
+        </div>
+        <div className="container-flight-details-price">
+          <p>££</p>
+          <button className="book-button">Book</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Flight;
