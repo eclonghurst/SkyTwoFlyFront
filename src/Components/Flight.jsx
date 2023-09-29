2e2cd767fa2d3d32465162102b70c03c4ae7dcb9
eclonghurst <emily.longhurst3@sky.uk>
2023-09-29 18:38:41 +0100

    flight reformat

--- src/Components/Flight.jsx
+++ src/Components/Flight.jsx
@@ -1,36 +1,26 @@
 import React from "react";
-import '../CssFiles/FlightCSS.css';
-import airlineLogo from '../Images/airlineLogo.png'
+import "../CssFiles/FlightCSS.css";
+import airlineLogo from "../Images/airlineLogo.png";
 
-
-function Flight(){
-
-    return(
-        <>
-        <div className='flight-panel'>
-            <div className='container-airline-details'>
-                <img src={airlineLogo} alt="Airline logo" className="airline-logo"/> 
-            </div>
-            <div className='container-flight-details'>
-            <div className='departure'>
-MAN
-            </div>
-            <div className='flight-pointer'>
-
-            </div>
-            <div className='destination'>
-DUS
-            </div>
-            </div>
-            <div className='container-flight-details-price'>
-<p>££</p>
-                    <button className='book-button'>Book</button>
-            </div>
-
-
+function Flight() {
+  return (
+    <>
+      <div className="flight-panel">
+        <div className="container-airline-details">
+          <img src={airlineLogo} alt="Airline logo" className="airline-logo" />
         </div>
-        </>
-    )
+        <div className="container-flight-details">
+          <div className="departure">MAN</div>
+          <div className="flight-pointer"></div>
+          <div className="destination">DUS</div>
+        </div>
+        <div className="container-flight-details-price">
+          <p>££</p>
+          <button className="book-button">Book</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Flight;
