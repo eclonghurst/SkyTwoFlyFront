3e079e95670a3bd3a135ee0d22d234d324abc8f2
Salma394 <salmaibrahim813@gmail.com>
2023-10-02 14:45:50 +0100

    toggle created for sign in and sign up

---
+++ src/Components/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
+import RegistrationForm from './RegistrationForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import '../CssFiles/LoginFormCSS.css';
+
+
+function Login(){
+    const[isVisible,setIsVisible] = useState(false);
+    const[isLoginForm,setIsLoginForm] = useState(true);
+    const toggleForm  = () => setIsLoginForm(!isLoginForm);
+    return (
+        <div>
+            <FontAwesomeIcon 
+                className="accountIcon" 
+                icon={faUser} 
+                style={{color: "#ffffff"}} 
+                onClick={() => setIsVisible(true)} 
+            />
+            { isVisible && (
+                <div className='backdrop' onClick={() => setIsVisible(false)}>
+                    <div className={`form-container`} onClick={(e) => e.stopPropagation()}>
+                        {isLoginForm ? 
+                            <LoginForm setIsVisible={setIsVisible}/> : 
+                            <RegistrationForm setIsVisible={setIsVisible}/>
+                        }
+                        <button className="toggle-form-button" onClick={toggleForm}>
+                            {isLoginForm ? "Don’t have an account? Register" : "Already have an account? Login"}
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import '../CssFiles/RegistrationFormCSS.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
