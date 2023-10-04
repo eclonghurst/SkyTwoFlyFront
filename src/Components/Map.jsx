9f1e54402868e66899b429fc84bc08ca12a0a07f
Fayequinn <fayequinn88@hotmail.com>
2023-10-04 15:08:31 +0100

    the map now renders the destination location

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -3,25 +3,31 @@ import { useState, useEffect, useRef } from "react";
 // import ReactMapGL from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../CssFiles/MapCSS.css";
+import airportJSON from "../airports.json";
 import mapboxgl from 'mapbox-gl';
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnItd2F0c29uIiwiYSI6ImNsbWYzbDE2cjI2YWIzZm14NjBzZGNtcmsifQ.ZYUdF_rHxMF7qdA93FndYA';
 
 
+
 function Map(props){
+
+  const airportCoordsDest = airportJSON.find(
+    (airportDest) => props.destination == airportDest.iata_code);
 
   const mapContainer = useRef(null);
 const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
+
+
+
+
 
 useEffect(() => {
   if (map.current) return; // initialize map only once
   map.current = new mapboxgl.Map({
   container: mapContainer.current,
   style: 'mapbox://styles/mapbox/streets-v12',
-  center: [lng, lat],
-  zoom: zoom
+  center: [airportCoordsDest.longitude_deg, airportCoordsDest.latitude_deg],
+  zoom: 9
   });
   });
 
