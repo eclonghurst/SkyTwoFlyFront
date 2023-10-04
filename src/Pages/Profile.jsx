e0cb58f334740e994588e60b54c2bb7fda0fcf96
Salma394 <salmaibrahim813@gmail.com>
2023-10-04 20:36:43 +0100

    editing profile ticket

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import FlightTicket from "../Components/FlightTicket";
+import { useLocation } from "react-router-dom";
 
 const Profile = () => {
+    const location = useLocation();
+    const user = location
     return(
         <>
+  
         <h1>Welcome to your profile page!</h1>
         <FlightTicket/>
         </>
