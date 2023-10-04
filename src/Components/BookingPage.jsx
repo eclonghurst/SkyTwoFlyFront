ac642290ca0f1109ee7e9a21b5da01c288f1ea27
eclonghurst <emily.longhurst3@sky.uk>
2023-10-04 12:14:08 +0100

    booking page works

--- src/Components/BookingPage.jsx
+++ src/Components/BookingPage.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import Flight from "./Flight";
 import { useLocation } from "react-router-dom";
 
-function BookingPage(){
+function BookingPage() {
+  const location = useLocation();
 
-    const location = useLocation();
+  console.log(location.state);
 
-    return(
-        <>
-        <Flight/>
-        <label for="confirm">Book GetYourWay flights today...     </label>
-        <button id="confirm">Confirm</button>
-        <div className="container-for-map">
-            <p>
-
-            </p>
-
-            <p>
-
-            </p>
-            <p>This is a container for the map. Map details for {location.state.destination} </p>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <Flight {...location.state} />
+      <label for="confirm">Book GetYourWay flights today... </label>
+      <button id="confirm">Confirm</button>
+      <div className="container-for-map">
+        <p></p>
+        <p></p>
+        <p>
+          This is a container for the map. Map details for{" "}
+          {location.state.destination}{" "}
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default BookingPage;
--- src/Components/HeroTagline.jsx
+++ src/Components/HeroTagline.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import '../CssFiles/HeroSectionCSS.css';
-
+import React from "react";
+import "../CssFiles/HeroSectionCSS.css";
 
 function HeroTagline() {
   return (
     <>
-    <div className="heroTaglineContainer">
-        <h1>Top Desinations in Autumn</h1>
-
-    </div>
+      <div className="heroTaglineContainer">
+        <h1>Top Destinations in Autumn</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default HeroTagline
+export default HeroTagline;
