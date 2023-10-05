61a17ec1ca00d9fada739bf703498df69e3cb089
eclonghurst <emily.longhurst3@sky.uk>
2023-10-05 18:19:36 +0100

    home page text start

--- src/Components/HeroText.jsx
+++ src/Components/HeroText.jsx
@@ -8,8 +8,8 @@ function HeroText() {
         <h2 className="heroText-header">Jet Off to the World’s Wonders!</h2>
         <h6 className="heroText-Body">
           Whether it's the serene beaches of Bali, the bustling streets of New
-          York, or the historical marvels of Rome, Sky UK brings the world
-          closer to you
+          York, or the historical marvels of Rome, GetYourWay brings the world
+          closer to you!
         </h6>
       </div>
     </div>
